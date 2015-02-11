9b7b0f9320f2d8800b7b60d0ee3c7e6625c99243
NickMattmiller <njmatt03@louisville.edu>
2015-02-11 01:21:05 -0500

    1.5

---
+++ MvcAppWeb/MvcAppWeb/Controllers/VaccineController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MvcAppWeb.Controllers
+{
+    public class VaccineController : Controller
+    {
+        //
+        // GET: /Vaccine/
+
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+    }
+}
